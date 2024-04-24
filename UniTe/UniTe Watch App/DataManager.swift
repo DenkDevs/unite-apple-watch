5c8d27e8a5904011b11bfc4eabd742777003508b
Nathan Tieu <ntieu3@gatech.edu>
2024-04-24 05:53:23 -0400

    functionality for only one data point

--- UniTe/UniTe Watch App/DataManager.swift
+++ UniTe/UniTe Watch App/DataManager.swift
@@ -11,42 +11,45 @@ import FirebaseDatabase
 
 class DataManager: ObservableObject {
     @Published var eventList: [Event] = []
-
-    let url = URL(string: "https://unite-fdcb6-default-rtdb.firebaseio.com/events.json")!
-
-    lazy var request = URLRequest(url: url)
     
-    lazy var task: URLSessionDataTask = {
-            var requestCopy = request
-            requestCopy.httpMethod = "GET"
-            
-            return URLSession.shared.dataTask(with: requestCopy) { data, response, error in
-                if let error = error {
-                    print(error)
-                    return
-                }
+    let url = URL(string: "https://us-central1-unite-fdcb6.cloudfunctions.net/api/get-all-events")!
+    lazy var session = URLSession.shared
+    
+    lazy var task = session.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print(error)
+            return
+        }
+
+//        if let rawDataString = String(data: data!, encoding: .utf8) {
+//                print("Raw data: \(rawDataString)")
+//            } else {
+//                print("Error converting data to string")
+//            }
+        
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: [String: Any]] {
+                print("Success")
+//                print("JSON Response: \(json)")
                 
-                do {
-                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: [String: String]] {
-                            print("Success")
-                            print("JSON Response: \(json)")
-                        
-                            for (_, event) in json {
-                                if let eventClub = event["clubName"],
-                                   let eventDescription = event["description"],
-                                   let eventTitle = event["title"],
-                                   let eventLocation = event["location"],
-                                   let eventTime = event["time"] {
-                                    let newEvent = Event(title: eventTitle, clubName: eventClub, location: eventLocation, time: eventTime, description: eventDescription)
-                                    self.eventList.append(newEvent)
-                                }
-                            }
-                        }
-                    } catch {
-                        print("Error parsing JSON: \(error)")
+                for (_, event) in json {
+                    print("event: \(event)")
+                    if let eventClub = event["clubName"],
+                       let eventDescription = event["description"],
+                       let eventTitle = event["title"],
+                       let eventLocation = event["location"],
+                       let eventTime = event["Time"] {
+                        let newEvent = Event(title: eventTitle as! String, clubName: eventClub as! String, location: eventLocation as! String, time: eventTime as! String, description: eventDescription as! String)
+                        self.eventList.append(newEvent)
                     }
+                }
             }
-        }()
+            
+//            print(self.eventList)
+        } catch {
+            print("Error parsing JSON: \(error)")
+        }
+    }
     
     init() {
         task.resume()
